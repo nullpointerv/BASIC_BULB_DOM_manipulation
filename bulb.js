fcd1da6683c6a633ad5085cf6a1d125844057acf
nullpointerv <113046267+nullpointerv@users.noreply.github.com>
2024-05-08 18:57:02 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,22 @@
+var bulb = document.querySelector(".bulb")
+var btn = document.querySelector("button")
+var flag= 0
+btn.addEventListener("click",function(){
+    if (flag == 0){
+        bulb.style.backgroundColor="yellow"
+ console.log("clicked")
+ btn.innerHTML='OFF'
+ flag=1
+
+    }else{
+        bulb.style.backgroundColor="white"
+        console.log("again clicked")
+ btn.innerHTML='ON'
+
+        flag=0
+
+    }
+
+
+
+})
